38f65620e4deb427b919fd2b2e5d3eb99e3e17ab
Funkyskep <phyrowolf24@gmail.com>
2024-11-21 09:11:35 -0500

    initail start

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import './App.css';
+
 
 function App() {
   return (
-    <>
+    <Router>
       <Nav />
       <main>
         <Outlet />
